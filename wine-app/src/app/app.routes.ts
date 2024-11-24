23d9cbe2e4c506d4570c0e6ae938276de02b0d6d
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2024-11-25 01:04:45 +0200

    Components
    components created, routing created, CSS created

--- wine-app/src/app/app.routes.ts
+++ wine-app/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { ErrorComponent } from './error/error.component';
+import { SearchComponent } from './main/search/search.component';
+import { CatalogComponent } from './main/catalog/catalog.component';
+import { CreateProductComponent } from './main/product/create-product/create-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent },
+    {path: 'auth', 
+        children: [
+            {path: 'login', component: LoginComponent},
+            {path: 'register', component: RegisterComponent},
+            {path: 'logout', component: HomeComponent},
+        ]
+    },
+    {path: 'catalog',
+        children: [
+            {path: '', component: CatalogComponent},
+            {path: 'search', component: SearchComponent},
+            {path: 'product',
+                children: [
+                    {path: 'create', component: CreateProductComponent},
+                ]
+            },
+
+        ]
+    },
+    
+    {
+        path: '**',
+        component: ErrorComponent,
+      },
+];
