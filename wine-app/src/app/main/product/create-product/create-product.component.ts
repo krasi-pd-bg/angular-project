f4096391c0b064bba6ae07669ccb087a5588a4f7
Krasimir Dimitrov <133798641+krasi-pd-bg@users.noreply.github.com>
2024-12-07 17:21:38 +0200

    getting data from create form

--- wine-app/src/app/main/product/create-product/create-product.component.ts
+++ wine-app/src/app/main/product/create-product/create-product.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.css'
 })
 export class CreateProductComponent {
+  errorMsg: string = '';
+  constructor(private router: Router) { }
 
+  create(form: NgForm) {
+    
+    if (form.invalid) {
+      this.errorMsg = 'All fields are required';
+      console.error('Invalid login form');      
+      return;
+    }
+
+    const { name, type, grapeVariety, vintage, wineCellar, regionCountry, price, description, image } = form.value;
+    
+
+    this.router.navigate(['/catalog']);
+    
+
+  }
 }
